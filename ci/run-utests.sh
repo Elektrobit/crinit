a2f63e0cffedbb2e72b16f0e0be9c2d8399a7c7c
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2022-01-27 17:48:13 +0100

    review: unit test script cleanup !10 #6272

--- ci/run-utests.sh
+++ ci/run-utests.sh
@@ -4,9 +4,9 @@
 #
 # Dependency: ci/build.sh needs to be run before
 #
-CMDPATH=$(cd $(dirname $0) && pwd)
+CMDPATH=$(cd "$(dirname "$0")" && pwd)
 BASEDIR=${CMDPATH%/*}
-UTEST_REPORT=$BASEDIR/result/utest_report.txt
+UTEST_REPORT="$BASEDIR"/result/utest_report.txt
 
 # check if ci/build.sh has been run before
 if  [ ! -f "/usr/aarch64-linux-gnu/lib/libcmocka.so" ] || \
@@ -18,22 +18,22 @@ fi
 
 # build tests
 make clean
-source $BASEDIR/ci/cross.env && make tests
+source "$BASEDIR"/ci/cross.env && make tests
 
-mkdir -p $BASEDIR/result/bin/aarch64/tests
-rm -f $UTEST_REPORT
+mkdir -p "$BASEDIR"/result/bin/aarch64/tests
+rm -f "$UTEST_REPORT"
 # run tests and copy artifacts
 set -o pipefail
 TESTERROR=false
 while IFS= read -r -d '' t; do
-    echo $(basename $t) | tee -a $UTEST_REPORT
-    cp $t $BASEDIR/result/bin/aarch64/tests/
+    basename "$t" | tee -a "$UTEST_REPORT"
+    cp "$t" "$BASEDIR"/result/bin/aarch64/tests/
     RETURNCODE=0
-    qemu-aarch64-static -L /usr/aarch64-linux-gnu $t 2>&1 | tee -a $UTEST_REPORT || RETURNCODE=$?
+    qemu-aarch64-static -L /usr/aarch64-linux-gnu "$t" 2>&1 | tee -a "$UTEST_REPORT" || RETURNCODE=$?
     if [ $RETURNCODE != 0 ]; then
         TESTERROR=true
     fi
-done < <(find $BASEDIR/test -executable -type f -print0)
+done < <(find "$BASEDIR"/test -executable -type f -print0)
 
 if [ $TESTERROR = true ]; then
     exit 1
