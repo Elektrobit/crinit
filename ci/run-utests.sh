697480861df4dece129bc3eef2ab38df391aa26a
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2022-01-27 16:49:36 +0100

    review: fix unit test script error handling !10 #6272

--- ci/run-utests.sh
+++ ci/run-utests.sh
@@ -6,6 +6,7 @@
 #
 CMDPATH=$(cd $(dirname $0) && pwd)
 BASEDIR=${CMDPATH%/*}
+UTEST_REPORT=$BASEDIR/result/utest_report.txt
 
 # check if ci/build.sh has been run before
 if  [ ! -f "/usr/aarch64-linux-gnu/lib/libcmocka.so" ] || \
@@ -20,7 +21,20 @@ make clean
 source $BASEDIR/ci/cross.env && make tests
 
 mkdir -p $BASEDIR/result/bin/aarch64/tests
+rm -f $UTEST_REPORT
 # run tests and copy artifacts
-find $BASEDIR/test -executable -type f -exec sh -c \
-    "echo \$(basename {}); cp {} $BASEDIR/result/bin/aarch64/tests/; qemu-aarch64-static -L /usr/aarch64-linux-gnu {}" \; 2>&1 \
-    | tee -a $BASEDIR/result/utest_report.txt
+set -o pipefail
+TESTERROR=false
+while IFS= read -r -d '' t; do
+    echo $(basename $t) | tee -a $UTEST_REPORT
+    cp $t $BASEDIR/result/bin/aarch64/tests/
+    RETURNCODE=0
+    qemu-aarch64-static -L /usr/aarch64-linux-gnu $t 2>&1 | tee -a $UTEST_REPORT || RETURNCODE=$?
+    if [ $RETURNCODE != 0 ]; then
+        TESTERROR=true
+    fi
+done < <(find $BASEDIR/test -executable -type f -print0)
+
+if [ $TESTERROR = true ]; then
+    exit 1
+fi
