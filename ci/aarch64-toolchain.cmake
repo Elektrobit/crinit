37f54a05a1ee6c12798d2a9bb50e93d352a69b91
Rainer Müller <rm@emlix.com>
2022-02-03 11:39:19 +0100

    clang-tidy: Fix execution with cmake build system #6944

--- ci/aarch64-toolchain.cmake
+++ ci/aarch64-toolchain.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
-set(CMAKE_C_FLAGS_INIT "-march=armv8-a+crc -mcpu=cortex-a53")
+set(CMAKE_C_FLAGS_INIT "-march=armv8-a+crc -mcpu=cortex-a53 -isystem /usr/aarch64-linux-gnu/include")
 set(CMAKE_AR aarch64-linux-gnu-ar)
 
 set(CMAKE_FIND_ROOT_PATH "/usr/aarch64-linux-gnu")
