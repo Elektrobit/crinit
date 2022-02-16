0f87e0eb62c12d083ec28f479f08faa029bb5489
Rainer Müller <rm@emlix.com>
2022-02-16 11:12:07 +0100

    crinit-client: Refactor EBCL_crinitXfer() #9925

--- inc/sockcom.h
+++ inc/sockcom.h
@@ -14,46 +14,22 @@
 #include "rtimcmd.h"
 
 /**
- * Connect to Crinit and wait for a ready-to-receive message.
+ * Perform a request/response transfer with Crinit
  *
- * @param sockFd    Return pointer for the connected socket.
+ * Will connect to Crinit and send a request/command, then receive the result/response.
+ * The server side equivalent is EBCL_connThread() in notiserv.c.
+ *
+ * The following image shows the high level communication sequence. For the lower level, refer to
+ * the internal functions EBCL_crinitSend() and EBCL_crinitRecv().
+ *
+ * \image html notiserv_sock_comm_seq.svg
+ *
  * @param sockFile  Path to the AF_UNIX socket file to connect to.
+ * @param res       Return pointer for response/result.
+ * @param cmd       The command/request to send.
  *
  * @return 0 on success, -1 otherwise
  */
-int EBCL_crinitConnect(int *sockFd, const char *sockFile);
-/**
- * Send a command/request to Crinit.
- *
- * Uses EBCL_rtimCmdToMsgStr() to generate a string and sends it using the same protocol as sendStr()/recvStr() in
- * notiserv.c. First, a binary size_t with the string size is sent, then the string itself in a second message/packet.
- *
- * The following diagram illustrates the low-level protocol:
- * \image html sock_comm_str.svg
- *
- * @param sockFd  The connected socket over which to send.
- * @param cmd     The command/request to send.
- *
- * @return 0 on success, -1 otherwise
- */
-int EBCL_crinitSend(int sockFd, const ebcl_RtimCmd_t *cmd);
-/**
- * Receive a response from Crinit.
- *
- * Receives a string using the same protocol as sendStr()/recvStr() in notiserv.c and then uses EBCL_parseRtimCmd() to
- * generate an equivalent ebcl_RtimCmd_t.
- *
- * First, a binary size_t with the string size is received, memory allocation made accordingly, and then the string
- * itself in a second message/packet is received.
- *
- * The following diagram illustrates the low-level protocol:
- * \image html sock_comm_str.svg
- *
- * @param sockFd  The connected socket from which to receive.
- * @param res     Return pointer for the response/result.
- *
- * @return 0 on success, -1 otherwise
- */
-int EBCL_crinitRecv(int sockFd, ebcl_RtimCmd_t *res);
+int EBCL_crinitXfer(const char *sockFile, ebcl_RtimCmd_t *res, const ebcl_RtimCmd_t *cmd);
 
 #endif /* __SOCKCOM_H__ */
