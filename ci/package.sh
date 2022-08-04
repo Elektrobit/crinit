e670ce06fb6cc682d052d70c01cf1cfdf80a6d49
Rainer Müller <rmueller@emlix.com>
2022-08-04 09:49:46 +0200

    ci: Create and copy debian package with debbuild

---
+++ ci/package.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+#
+# project build script
+#
+# Usage: ./ci/package.sh [Release|Debug]
+#
+CMDPATH=$(cd "$(dirname "$0")" && pwd)
+BASEDIR=${CMDPATH%/*}
+
+# architecture name amd64, arm64, ...
+ARCH=$(dpkg --print-architecture)
+# architecture name x86_64, aarch64, ...
+ARCH_ALT=$(uname -m)
+
+BUILD_TYPE="Release"
+if [ -n "$1" ]; then
+    BUILD_TYPE="$1"
+    shift
+fi
+
+case "$BUILD_TYPE" in
+    Release)
+        BUILDDIR="$BASEDIR/build/$ARCH"
+        RESULTDIR="$BASEDIR/result/$ARCH"
+        ;;
+    *)
+        BUILDDIR="$BASEDIR/build/$ARCH-$BUILD_TYPE"
+        RESULTDIR="$BASEDIR/result/$ARCH-$BUILD_TYPE"
+        ;;
+esac
+
+"$CMDPATH"/build.sh "$BUILD_TYPE"
+
+# prepare result dir
+mkdir -p "$RESULTDIR"/deb
+
+# package
+cd $BASEDIR
+
+# create debian packages
+make -C "$BUILDDIR" debbuild
+
+# copy packages to result dir
+cp "$BUILDDIR"/debbuild/SDEBS/*.sdeb "$RESULTDIR"/deb/
+cp "$BUILDDIR"/debbuild/DEBS/*/*.deb "$RESULTDIR"/deb/
