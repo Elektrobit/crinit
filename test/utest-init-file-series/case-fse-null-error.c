e34f68fe4406712dccdae1d8d215025e9f8e6a92
Andreas Schickedanz <as@emlix.com>
2023-01-25 08:44:26 +0100

    test: Add tests for EBCL_initFileSeries
    Signed-off-by: Andreas Schickedanz <as@emlix.com>

---
+++ test/utest-init-file-series/case-fse-null-error.c
@@ -0,0 +1,25 @@
+/**
+ * @file case-fse-null-error.c
+ * @brief Unit test for EBCL_envSetDestroy(), given file series is NULL.
+ *
+ * @author emlix GmbH, 37083 Göttingen, Germany
+ *
+ * @copyright 2023 Elektrobit Automotive GmbH
+ *            All rights exclusively reserved for Elektrobit Automotive GmbH,
+ *            unless otherwise expressly agreed
+ */
+
+#include <stdio.h>
+
+#include "common.h"
+#include "fseries.h"
+#include "unit_test.h"
+
+
+void EBCL_initFileSeriesTestFseNullError(void **state) {
+    EBCL_PARAM_UNUSED(state);
+
+    expect_any(__wrap_EBCL_errPrintFFL, format);
+
+    assert_int_equal(EBCL_initFileSeries(NULL, 0, NULL), -1);
+}
