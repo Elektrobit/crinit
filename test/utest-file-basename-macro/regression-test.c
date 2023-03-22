840bdf83746ee05cd501efc2c7e46b3621ceecf1
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2023-03-22 12:47:25 +0100

    test: add regression test for file basename macro behavior #17626

---
+++ test/utest-file-basename-macro/regression-test.c
@@ -0,0 +1,29 @@
+/**
+ * @file regression-test.c
+ * @brief Implementation of a regression test to check the __FILE_BASENAME__ macro.
+ *
+ * @author emlix GmbH, 37083 Göttingen, Germany
+ *
+ * @copyright 2021-2023 Elektrobit Automotive GmbH
+ *            All rights exclusively reserved for Elektrobit Automotive GmbH,
+ *            unless otherwise expressly agreed
+ */
+#include <libgen.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+#include "logio.h"
+#include "unit_test.h"
+#include "utest-file-basename-macro.h"
+
+void EBCL_fileBasenameMacroRegressionTest(void **state) {
+    EBCL_PARAM_UNUSED(state);
+
+    // Check that __FILE_BASENAME__ is indeed the basename of __FILE__
+    // We need a working copy as POSIX does not guarantee that basename does not modify its argument.
+    char *filepath = strdup(__FILE__);
+    assert_ptr_not_equal(filepath, NULL);
+    assert_int_equal(strcmp(basename(filepath), __FILE_BASENAME__), 0);
+    free(filepath);
+}
---
+++ test/utest-file-basename-macro/utest-file-basename-macro.c
@@ -0,0 +1,23 @@
+/**
+ * @file utest-file-basename-macro.c
+ * @brief Implementation of the unit test group for the __FILE_BASENAME__ macro.
+ *
+ * @author emlix GmbH, 37083 Göttingen, Germany
+ *
+ * @copyright 2021-2023 Elektrobit Automotive GmbH
+ *            All rights exclusively reserved for Elektrobit Automotive GmbH,
+ *            unless otherwise expressly agreed
+ */
+
+#include "utest-file-basename-macro.h"
+
+#include "unit_test.h"
+
+/**
+ * Runs the unit test group for EBCL_crinitSetVerbose using the cmocka API.
+ */
+int main(void) {
+    const struct CMUnitTest tests[] = {cmocka_unit_test(EBCL_fileBasenameMacroRegressionTest)};
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
