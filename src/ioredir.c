64a2969401253dd7b56c7ba108690ec8e9254453
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2023-04-12 10:13:33 +0200

    ioredir: refactor logic of IO redir config line parsing #17644

--- src/ioredir.c
+++ src/ioredir.c
@@ -59,74 +59,69 @@ int EBCL_initIoRedirFromConfKvList(ebcl_IoRedir_t *out, const char *key, size_t 
 
     if (strcmp(confStrArr[1], EBCL_CONFIG_STDOUT_NAME) == 0) {
         out->oldFd = STDOUT_FILENO;
-    }
-    if (strcmp(confStrArr[1], EBCL_CONFIG_STDERR_NAME) == 0) {
+    } else if (strcmp(confStrArr[1], EBCL_CONFIG_STDERR_NAME) == 0) {
         out->oldFd = STDERR_FILENO;
-    }
-    if (strcmp(confStrArr[1], EBCL_CONFIG_STDIN_NAME) == 0) {
+    } else if (strcmp(confStrArr[1], EBCL_CONFIG_STDIN_NAME) == 0) {
         out->oldFd = STDIN_FILENO;
-    }
-    if (out->oldFd != -1) {
-        EBCL_freeArgvArray(confStrArr);
-        return 0;
-    }
-
-    // Assume it's a path if it wasn't either STDOUT, STDERR, or STDIN.
-    if (!EBCL_isAbsPath(confStrArr[1])) {
-        EBCL_errPrint("Path to file for IO redirection must be absolute.");
-        EBCL_freeArgvArray(confStrArr);
-        return -1;
-    }
-    out->path = strdup(confStrArr[1]);
-    if (out->path == NULL) {
-        EBCL_errnoPrint("Could not duplicate path string during parsing of IO redirection statement.");
-        EBCL_freeArgvArray(confStrArr);
-        return -1;
-    }
-
-    if (numParams > 2) {
-        if (strcmp(confStrArr[2], "TRUNCATE") == 0) {
-            out->oflags = O_TRUNC | O_CREAT;
-        } else if (strcmp(confStrArr[2], "APPEND") == 0) {
-            out->oflags = O_APPEND | O_CREAT;
-        } else {
-            EBCL_errPrint(
-                "Third parameter of redirection statement - if it is given - must be either APPEND or TRUNCATE.");
+    } else if (EBCL_isAbsPath(confStrArr[1])) {
+        out->path = strdup(confStrArr[1]);
+        if (out->path == NULL) {
+            EBCL_errnoPrint("Could not duplicate path string during parsing of IO redirection statement.");
             EBCL_freeArgvArray(confStrArr);
-            free(out->path);
-            out->path = NULL;
             return -1;
         }
-    }
-
-    if (numParams > 3) {
-        char *endPtr;
-        out->mode = strtoul(confStrArr[3], &endPtr, 8);
-        if (out->mode == 0 && (errno != 0 || endPtr == confStrArr[3])) {
-            if (errno == 0) {
-                EBCL_errPrint(
-                    "Encountered token that is not an octal number for file mode in IO redirection statement.");
-            } else {
-                EBCL_errnoPrint("Could not perform conversion of octal mode digits in IO redirection statement.");
-            }
-            EBCL_freeArgvArray(confStrArr);
-            free(out->path);
-            out->path = NULL;
-            return -1;
-        }
-        if (out->mode > 0777) {
-            EBCL_errPrint("0%o is not a supported file mode.", out->mode);
-            EBCL_freeArgvArray(confStrArr);
-            free(out->path);
-            out->path = NULL;
-            return -1;
-        }
-    }
-
-    if (out->newFd == STDIN_FILENO) {
-        out->oflags = O_RDONLY;
     } else {
-        out->oflags |= O_WRONLY;
+        EBCL_errPrint("Redirection target must be a standard file descriptor or an absolute path.");
+        EBCL_freeArgvArray(confStrArr);
+        return -1;
+    }
+
+    // The rest of the parameters/flags are only relevant if a file will be opened.
+    if (out->path != NULL) {
+        if (numParams > 2) {
+            if (strcmp(confStrArr[2], "TRUNCATE") == 0) {
+                out->oflags = O_TRUNC | O_CREAT;
+            } else if (strcmp(confStrArr[2], "APPEND") == 0) {
+                out->oflags = O_APPEND | O_CREAT;
+            } else {
+                EBCL_errPrint(
+                    "Third parameter of redirection statement - if it is given - must be either APPEND or TRUNCATE.");
+                EBCL_freeArgvArray(confStrArr);
+                free(out->path);
+                out->path = NULL;
+                return -1;
+            }
+        }
+
+        if (numParams > 3) {
+            char *endPtr;
+            out->mode = strtoul(confStrArr[3], &endPtr, 8);
+            if (out->mode == 0 && (errno != 0 || endPtr == confStrArr[3])) {
+                if (errno == 0) {
+                    EBCL_errPrint(
+                        "Encountered token that is not an octal number for file mode in IO redirection statement.");
+                } else {
+                    EBCL_errnoPrint("Could not perform conversion of octal mode digits in IO redirection statement.");
+                }
+                EBCL_freeArgvArray(confStrArr);
+                free(out->path);
+                out->path = NULL;
+                return -1;
+            }
+            if (out->mode > 0777) {
+                EBCL_errPrint("0%o is not a supported file mode.", out->mode);
+                EBCL_freeArgvArray(confStrArr);
+                free(out->path);
+                out->path = NULL;
+                return -1;
+            }
+        }
+
+        if (out->newFd == STDIN_FILENO) {
+            out->oflags = O_RDONLY;
+        } else {
+            out->oflags |= O_WRONLY;
+        }
     }
 
     EBCL_freeArgvArray(confStrArr);
