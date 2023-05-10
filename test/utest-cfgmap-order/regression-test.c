502465fc7dcb2b554ddf970037474b60af0351f4
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2023-05-10 18:44:21 +0200

    test: Add regression test for EBCL_cfgMap order/size #17860

---
+++ test/utest-cfgmap-order/regression-test.c
@@ -0,0 +1,29 @@
+/**
+ * @file regression-test.c
+ * @brief Implementation of a regression test for the order/size of EBCL_cfgMap.
+ *
+ * @author emlix GmbH, 37083 Göttingen, Germany
+ *
+ * @copyright 2023 Elektrobit Automotive GmbH
+ *            All rights exclusively reserved for Elektrobit Automotive GmbH,
+ *            unless otherwise expressly agreed
+ */
+#include <string.h>
+
+#include "common.h"
+#include "confmap.h"
+#include "unit_test.h"
+#include "utest-cfgmap-order.h"
+
+void EBCL_cfgMapRegressionTest(void **state) {
+    EBCL_PARAM_UNUSED(state);
+
+    // Check size of EBCL_cfgMap.
+    assert_int_equal(EBCL_cfgMapSize, EBCL_CONFIGS_SIZE);
+
+    // Check order
+    for (size_t i = 1; i < EBCL_cfgMapSize; i++) {
+        // We are correctly ordered if each following configKey is lexicographically larger than the one before.
+        assert_true(strcmp(EBCL_cfgMap[i].configKey, EBCL_cfgMap[i - 1].configKey) > 0);
+    }
+}
---
+++ test/utest-cfgmap-order/utest-cfgmap-order.h
@@ -0,0 +1,22 @@
+/**
+ * @file utest-cfgmap-order.h
+ * @brief Header declaring the regression test for the order/size of EBCL_cfgMap.
+ *
+ * @author emlix GmbH, 37083 Göttingen, Germany
+ *
+ * @copyright 2023 Elektrobit Automotive GmbH
+ *            All rights exclusively reserved for Elektrobit Automotive GmbH,
+ *            unless otherwise expressly agreed
+ */
+#ifndef __UTEST_CFGMAP_ORDER_H__
+#define __UTEST_CFGMAP_ORDER_H__
+
+/**
+ * Regression test for the EBCL_cfgMap constant array.
+ *
+ * Checks that the array is alphabetically ordered (by its ebcl_ConfigMapping_t::configKey field) and that it contains
+ * EBCL_CONFIGS_SIZE number of elements.
+ */
+void EBCL_cfgMapRegressionTest(void **state);
+
+#endif /* __UTEST_CFGMAP_ORDER__ */
