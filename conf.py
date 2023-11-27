e18ce70ed2cce5d4fa26140d19dd563af6f7ddce
Anton Hillerband <ahille@emlix.com>
2023-11-27 07:37:48 +0100

    Building Doc outside of matrix, after build steps
    Signed-off-by: Anton Hillerband <ahille@emlix.com>

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ extensions = [
 ]
 
 templates_path = ['doc/_templates']
-exclude_patterns = ['deps/*', 'packaging/**', "build/**/README.md", "build/amd64/debbuild/**", "result/**", "build/amd64/doc/doxygen/doc/adr/**", "build/amd64/doc/sphinx/**", "build/amd64-Debug/**"]
+exclude_patterns = ['deps/*', 'packaging/**', "build/**/README.md", "build/*/debbuild/**", "result/**", "build/*/doc/doxygen/doc/adr/**", "build/*/doc/sphinx/**", "build/*-Debug/**"]
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
