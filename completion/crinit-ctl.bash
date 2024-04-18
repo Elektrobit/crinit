272b15280d874b758c63a3369bb3c14cd5fb2697
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2024-04-18 11:10:01 +0200

    compl: refactor to fix shellcheck warnings

--- completion/crinit-ctl.bash
+++ completion/crinit-ctl.bash
@@ -2,12 +2,18 @@
 #
 # crinit-ctl bash completion script
 
+_compreply_add() {
+    while IFS='' read -r line || [[ -n ${line} ]];
+        do COMPREPLY+=("${line}");
+    done < <(eval "$1")
+}
+
+_add_static_options() {
+    _compreply_add "compgen -W '$1' -- '${cur}'"
+}
+
 _add_fname_completions_filtered() {
-    paths=$(compgen -o plusdirs -f -X "$1" -- "${cur}")
-    # line-by-line parsing necessary to handle whitespace filenames correctly
-    while IFS= read -r entry || [[ -n ${entry} ]]; do
-        COMPREPLY+=("${entry}")
-    done < <(printf '%s' "${paths}")
+    _compreply_add "compgen -o plusdirs -f -X '$1' -- '${cur}'"
 }
 
 _find_base_comp_words_index() {
@@ -18,12 +24,13 @@ _find_base_comp_words_index() {
     done
 }
 
+
 _crinit-ctl() {
-    local cur base action opts
+    local cur action
     compopt +o default
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    actindex=$(($(_find_base_comp_words_index $1)+1))
+    actindex=$(($(_find_base_comp_words_index "$1")+1))
     action="${COMP_WORDS[${actindex}]}"
     verbs="--help
         --version
@@ -41,29 +48,26 @@ _crinit-ctl() {
         poweroff"
 
     if [[ ${COMP_CWORD} -eq ${actindex} ]]; then
-        COMPREPLY=( $(compgen -W "${verbs}" -- "${cur}") )
+        _add_static_options "${verbs}"
         return 0
     fi
 
     case "${action}" in
         addtask)
-            local opts="--ignore-deps --override-deps --overwrite --verbose"
-            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            _add_static_options "--ignore-deps --override-deps --overwrite --verbose"
             _add_fname_completions_filtered "!*.crinit"
             ;;
         addseries)
-            local opts="--overwrite --verbose"
-            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            _add_static_options "--overwrite --verbose"
             _add_fname_completions_filtered "!*.series"
             ;;
         enable|disable|stop|kill|restart|status|notify)
-            local opts="--verbose $(crinit-ctl list | tail -n +2 | cut -f1 -d ' ')"
-            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            _add_static_options "--verbose $(crinit-ctl list | tail -n +2 | cut -f1 -d ' ')"
             ;;
         *)
-            COMPREPLY=( "--verbose" )
+            _add_static_options "--verbose"
     esac
-    [[ ${COMP_CWORD} -ne ${actindex} ]] && [[ "${COMPREPLY}" != "-*" ]] && compopt -o filenames
+    [[ ${COMP_CWORD} -ne ${actindex} ]] && [[ "${COMPREPLY[0]}" != "-*" ]] && compopt -o filenames
 }
 
 complete -F _crinit-ctl crinit-ctl
