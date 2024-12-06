a8e8160968016eac8ccca7eed8aae5cd5d7695d0
Björn Kirchner <bk@emlix.com>
2024-12-06 12:30:58 +0100

    Add unit tests for the "USER" configuration key handler

---
+++ test/utest/utest-crinit-cfg-user-handler/case-negative-input.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+/**
+ * @file case-negative-input.c
+ * @brief Unit test for crinitCfgUserHandler(), handling of negative input.
+ */
+
+#include "confhdl.h"
+#include "common.h"
+#include "unit_test.h"
+#include "utest-crinit-cfg-user-handler.h"
+
+#include <string.h>
+
+void crinitCfgUserHandlerTestNegativeInput(void **state) {
+    CRINIT_PARAM_UNUSED(state);
+
+    crinitTask_t tgt;
+    memset(&tgt, 0x00, sizeof(tgt));
+    const char *val = "-42";
+    assert_int_equal(crinitCfgUserHandler(&tgt, val, CRINIT_CONFIG_TYPE_TASK), -1);
+    assert_int_equal(tgt.user, 0);
+}
---
+++ test/utest/utest-crinit-cfg-user-handler/utest-crinit-cfg-user-handler.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+/**
+ * @file utest-crinit-cfg-user-handler.c
+ * @brief Implementation of the crinitCfgUserHandler() unit test group.
+ */
+
+#include "utest-crinit-cfg-user-handler.h"
+
+#include "unit_test.h"
+
+/**
+ * Runs the unit test group for crinitCfgUserHandler() using the cmocka API.
+ */
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(crinitCfgUserHandlerTestNumericSuccess),
+        cmocka_unit_test(crinitCfgUserHandlerTestAlphaInput),
+        cmocka_unit_test(crinitCfgUserHandlerTestNegativeInput),
+        cmocka_unit_test(crinitCfgUserHandlerTestNullInput),
+        cmocka_unit_test(crinitCfgUserHandlerTestEmptyInput),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
