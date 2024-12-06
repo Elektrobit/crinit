947cc145e78be6b7279fea3ff0fa47bbc8faf512
Björn Kirchner <bk@emlix.com>
2024-12-06 12:30:37 +0100

    Add unit tests for the "GROUP" configuration key handler

---
+++ test/utest/utest-crinit-cfg-group-handler/utest-crinit-cfg-group-handler.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+/**
+ * @file utest-crinit-cfg-group-handler.c
+ * @brief Implementation of the crinitCfgGroupHandler() unit test group.
+ */
+
+#include "utest-crinit-cfg-group-handler.h"
+
+#include "unit_test.h"
+
+/**
+ * Runs the unit test group for crinitCfgGroupHandler() using the cmocka API.
+ */
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(crinitCfgGroupHandlerTestNumericSuccess),
+        cmocka_unit_test(crinitCfgGroupHandlerTestAlphaInput),
+        cmocka_unit_test(crinitCfgGroupHandlerTestNegativeInput),
+        cmocka_unit_test(crinitCfgGroupHandlerTestNullInput),
+        cmocka_unit_test(crinitCfgGroupHandlerTestEmptyInput),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
