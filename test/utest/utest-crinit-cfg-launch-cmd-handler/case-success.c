d5026b27ac8fa88f96880294f5acf7f1bc6fc560
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2025-02-19 15:40:05 +0100

    utest: fix path issues in launch cmd test

--- test/utest/utest-crinit-cfg-launch-cmd-handler/case-success.c
+++ test/utest/utest-crinit-cfg-launch-cmd-handler/case-success.c
@@ -4,25 +4,26 @@
  * @brief Unit test for crinitCfgLauncherCmdHandler(), successful execution.
  */
 
-#include "confhdl.h"
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
+#include "confhdl.h"
 #include "globopt.h"
 #include "unit_test.h"
 #include "utest-crinit-cfg-launch-cmd-handler.h"
 
-#include <stdlib.h>
-#include <string.h>
+#define TRUE_CMD "/bin/true"
 
 void crinitCfgLauncherCmdHandlerTestExistingExecutableSuccess(void **state) {
     CRINIT_PARAM_UNUSED(state);
 
     char *launcher = NULL;
     assert_int_equal(crinitGlobOptInitDefault(), 0);
-    const char *val = "/usr/bin/true";
-    assert_int_equal(crinitCfgLauncherCmdHandler(NULL, val, CRINIT_CONFIG_TYPE_SERIES), 0);
+    assert_int_equal(crinitCfgLauncherCmdHandler(NULL, TRUE_CMD, CRINIT_CONFIG_TYPE_SERIES), 0);
     assert_int_equal(crinitGlobOptGet(CRINIT_GLOBOPT_LAUNCHER_CMD, &launcher), 0);
     assert_non_null(launcher);
-    assert_string_equal("/usr/bin/true", launcher);
+    assert_string_equal(TRUE_CMD, launcher);
     crinitGlobOptDestroy();
     free(launcher);
 }
