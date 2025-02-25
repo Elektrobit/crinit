e8d0748fea3094897fb0c0f7eef17011734e3da4
Björn Kirchner <bk@emlix.com>
2025-02-25 10:23:50 +0100

    feat(elos): add unit tests for new ELOS poll interval config handler

---
+++ test/utest/utest-crinit-cfg-elos-poll-interval-handler/case-empty-input.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+/**
+ * @file case-empty-input.c
+ * @brief Unit test for crinitCfgElosPollIntervalHandler(), handling of empty input.
+ */
+
+#include <string.h>
+
+#include "common.h"
+#include "confhdl.h"
+#include "globopt.h"
+#include "unit_test.h"
+#include "utest-crinit-cfg-elos-poll-interval-handler.h"
+
+void crinitCfgElosPollIntervalHandlerTestEmptyInput(void **state) {
+    CRINIT_PARAM_UNUSED(state);
+
+    const char *val = "";
+    assert_int_equal(crinitGlobOptInitDefault(), 0);
+    assert_int_equal(crinitCfgElosPollIntervalHandler(NULL, val, CRINIT_CONFIG_TYPE_SERIES), -1);
+    crinitGlobOptDestroy();
+}
