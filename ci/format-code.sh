8de8e6e27b7dd76ce1a444f3fb3e7a082de1c455
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2025-04-02 17:13:12 +0200

    build(ci): add python support to auto-formatter

--- ci/format-code.sh
+++ ci/format-code.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 #
-# Code formatter and formatting checker script. Uses shfmt (sh/bash) and
-# clang-format (C).
+# Code formatter and formatting checker script. Uses shfmt (sh/bash),
+# clang-format (C), and yapf (Python).
 #
 # Usage: ./ci/format-code.sh [--check]
 #
@@ -10,14 +10,15 @@
 # if file changes would be necessary. This funcitonality is mainly meant for
 # the CI.
 #
-# Set CLANG_FORMAT_CMD and/or SHFMT_CMD env variable if your clang-format
-# and/or shfmt tools are somewhere else than ${PATH}.
+# Set CLANG_FORMAT_CMD, SHFMT_CMD and/or YAPF_CMD env variables if your code
+# formatting tools are called different or not in ${PATH}.
 #
 
 set -euo pipefail
 
 CLANG_FORMAT_CMD=${CLANG_FORMAT_CMD:-clang-format}
 SHFMT_CMD=${SHFMT_CMD:-shfmt}
+YAPF_CMD=${YAPF_CMD:-yapf3}
 CMDPATH=$(cd "$(dirname "$0")" && pwd)
 BASEDIR=${CMDPATH%/*}
 
@@ -30,22 +31,26 @@ print_usage() {
 
 # Google shell style for shfmt with 4 space indentation instead of 2.
 SHFMT_STYLE_ARGS="-i 4 -bn -ci"
-# For clang-format, we just use our existing style file.
+# For clang-format and yapf, we just use our existing style files.
 CLANG_FORMAT_STYLE_ARGS="-style=file:${BASEDIR}/.clang-format"
+YAPF_STYLE_ARGS="--style ${BASEDIR}/.style.yapf"
 
 # By default edit the files:
 SHFMT_ACTION_ARGS="-w"
 CLANG_FORMAT_ACTION_ARGS="-i"
+YAPF_ACTION_ARGS="-i"
 
 # The directories we want to search in for both tools.
 C_CODE_DIRS="${BASEDIR}/inc ${BASEDIR}/src"
 SH_CODE_DIRS="${BASEDIR}/ci ${BASEDIR}/scripts ${BASEDIR}/test"
+PY_CODE_DIRS="${BASEDIR}" # We have conf.py in the root dir.
 
 if [[ $# == 1 ]]; then
     if [ "$1" = "--check" ]; then
         # do dry-runs instead
         SHFMT_ACTION_ARGS="-l"                # Will list files that would be changed.
         CLANG_FORMAT_ACTION_ARGS="-n -Werror" # Will do a dry-run and report error if files need to be changed.
+        YAPF_ACTION_ARGS="-q"                 # Will do a dry-run and report error if files need to be changed.
     else
         print_usage
         exit 1
@@ -69,6 +74,13 @@ if [[ "$(find ${SH_CODE_DIRS} -iname '*.sh' -print0 \
     | xargs -0 "${SHFMT_CMD}" ${SHFMT_STYLE_ARGS} ${SHFMT_ACTION_ARGS})" ]]; then
     FAIL=1
     echo Shfmt found formatting issues in some shell script files, please fix. 1>&2
+fi
+
+# shellcheck disable=SC2086 # Intended splitting of YAPF_STYLE_ARGS, YAPF_ACTION_ARGS
+if ! find ${PY_CODE_DIRS} -iname '*.py' -print0 \
+    | xargs -0 "${YAPF_CMD}" ${YAPF_STYLE_ARGS} ${YAPF_ACTION_ARGS}; then
+    FAIL=1
+    echo Yapf found formatting issues in some Python files, please fix. 1>&2
 fi
 
 exit ${FAIL}
