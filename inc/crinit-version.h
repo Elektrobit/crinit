42dec2c37a30803999537dc3330a2bea0257af7e
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2025-04-03 15:38:33 +0200

    feat: rename version public header to crinit-version to avoid clashes
    We also rename the related C source file to keep their naming aligned.

--- inc/crinit-version.h
+++ inc/crinit-version.h
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 /**
- * @file version.h
+ * @file crinit-version.h
  * @brief Header definitions related to component versioning.
  */
-#ifndef __VERSION_H__
-#define __VERSION_H__
+#ifndef __CRINIT_VERSION_H__
+#define __CRINIT_VERSION_H__
 
 #include <stdint.h>
 
@@ -24,18 +24,18 @@ typedef struct crinitVersion_t {
 } crinitVersion_t;
 
 /**
- * Crinit software component version. Generated via CMake in version.c.in.
+ * Crinit software component version. Generated via CMake in crinit-version.c.in.
  */
 extern const crinitVersion_t crinitVersion;
 
 /**
  * Returns a const char pointer to a formatted human-readable version string for this software component.
  *
- * Generated via CMake in version.c.in. Format is `MAJOR.MINOR.MICRO.GIT` if a git commit hash exists or
+ * Generated via CMake in crinit-version.c.in. Format is `MAJOR.MINOR.MICRO.GIT` if a git commit hash exists or
  * `MAJOR.MINOR.MICRO` if the git hash is an empty string.
  *
  * @return  Pointer to a version string constant.
  */
 const char *crinitGetVersionString(void);
 
-#endif /* __VERSION_H__ */
+#endif /* __CRINIT_VERSION_H__ */
