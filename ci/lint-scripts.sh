9a7cae33f592cc2854e89e7c91bb5262b4f4d5f0
Andreas Zdziarstek <andreas.zdziarstek@emlix.com>
2025-04-11 23:02:19 +0200

    build: add shellcheck to build container and linter script

--- ci/lint-scripts.sh
+++ ci/lint-scripts.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 #
-# Script linter tool, currently for Python, shellcheck planned.
+# Script linter tool for Python and sh/bash.
 #
 # Usage: ./ci/lint-scripts.sh
 #
 # The --check parameter causes the script to only do a dry-run and error out
-# if file changes would be necessary. This funcitonality is mainly meant for
+# if file changes would be necessary. This functionality is mainly meant for
 # the CI.
 #
-# Set FLAKE8_CMD env variable if your code linting tool is called differently
-# or not in ${PATH}.
+# Set FLAKE8_CMD and/or SHELLCHK_CMD env variables if your code linting tools
+# (flake8 and shellcheck) are called differently  or not available in ${PATH}.
 #
 
 set -euo pipefail
 
 FLAKE8_CMD=${FLAKE8_CMD:-flake8}
+SHELLCHK_CMD=${SHELLCHK_CMD:-shellcheck}
 CMDPATH=$(cd "$(dirname "$0")" && pwd)
 BASEDIR=${CMDPATH%/*}
+
+# The dirs we want to search for sh files. Not needed for python as flake8
+# will crawl all subdirs by default.
+SH_CODE_DIRS="${BASEDIR}/ci ${BASEDIR}/scripts ${BASEDIR}/test"
+# Let shellcheck follow source statements.
+SHELLCHK_ARGS="-x"
 
 # Use our repository Flake8 configuration (e.g. for custom line width)
 FLAKE8_CONF_ARGS="--config ${BASEDIR}/.flake8"
@@ -25,6 +32,7 @@ FLAKE8_CONF_ARGS="--config ${BASEDIR}/.flake8"
 FAIL=0
 
 pop_back() {
+    # shellcheck disable=SC2317 # False positive "unreachable statement" due to trap.
     popd >/dev/null
 }
 
@@ -35,6 +43,12 @@ trap pop_back EXIT
 if ! ${FLAKE8_CMD} ${FLAKE8_CONF_ARGS}; then
     FAIL=1
     echo Flake8 found the above issues in Python code, please fix. 1>&2
+fi
+
+# shellcheck disable=SC2086 # Intended splitting of SH_CODE_DIRS, SHELLCHK_ARGS.
+if ! find ${SH_CODE_DIRS} -iname '*.sh' -print0 | xargs -0 "${SHELLCHK_CMD}" ${SHELLCHK_ARGS}; then
+    FAIL=1
+    echo Shellcheck found the above issues in sh/bash code, please fix. 1>&2
 fi
 
 exit ${FAIL}
