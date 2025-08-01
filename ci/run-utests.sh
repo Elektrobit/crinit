cc1924dbec0fe258ae9f393239686c5ba1636f78
Wolfgang Gehrhardt <wolfgang.gehrhardt@emlix.com>
2025-08-01 17:37:53 +0200

    feat(utest): add option to run selective tests
    This align the run-utests.sh script with the one used in elos or other
    projects.
    
    * new switch -R or --tests-regex to run a selection of tests
    * adjust help and doc string
    * add log about skipped tests

--- ci/run-utests.sh
+++ ci/run-utests.sh
@@ -1,29 +1,59 @@
 #!/bin/bash -e
 # SPDX-License-Identifier: MIT
-#
-# script to build and run unit tests
-#
-# Dependency: ci/build.sh needs to be run before
-#
+###############################################################################
+print_info() {
+    echo "
+    Run the unit test suite or parts of it. Default is to run all unit
+    tests for the Debug build.
+
+    Usage: ${0} [build type] [--test-regex|-R <test name pattern>] [-h|--help]
+
+    build type:       usually Debug or Release but can be any other build type
+    --test-regex|-R:  execute all tests matching the pattern
+    -h|--help:        print this help
+
+    Examples:
+    ${0} Release # run all unit test on Release build
+    ${0} Release -R resize # run all unit test containing 'resize' in
+    the name for the Release build.
+    "
+}
+###############################################################################
+set -e -u -o pipefail
 CMDPATH=$(cd "$(dirname "$0")" && pwd)
 BASEDIR=${CMDPATH%/*}
 
-if [ $# -gt 1 ]; then
-    echo "error: only one build-type allowed"
-    exit 1
-fi
+TESTS_REGEX=""
+PARAM=""
+while [ $# -gt 0 ]; do
+    case ${1} in
+        --tests-regex | -R)
+            TESTS_REGEX="--tests-regex ${2}"
+            shift
+            ;;
+        -h | --help)
+            print_info
+            exit 0
+            ;;
+        -*)
+            echo "error: unknown option: $1"
+            print_info
+            exit 1
+            ;;
+        *)
+            PARAM="$PARAM ${1}"
+            ;;
+    esac
+    shift
+done
+# shellcheck disable=SC2086 # Intended splitting of PARAM.
+set -- $PARAM
 
-BUILD_TYPE="${1}"
-if [ $# -eq 0 ]; then
-    BUILD_TYPE="Debug"
-fi
+BUILD_TYPE="${1:-Debug}"
 
 BUILDDIR="${BASEDIR}/build/${BUILD_TYPE}"
 CMAKE_BUILD_DIR="${BUILDDIR}/cmake"
-RESULTDIR="${BUILDDIR}/result/"
-
-UTEST_REPORT="$RESULTDIR"/utest_report.txt
-UTEST_LOG="$RESULTDIR"/LastTest.log
+RESULT_DIR="${BUILDDIR}/result/"
 
 # check if ci/build.sh has been run before
 if [ ! -d "$CMAKE_BUILD_DIR" ]; then
@@ -31,12 +61,26 @@ if [ ! -d "$CMAKE_BUILD_DIR" ]; then
     exit 1
 fi
 
-rm -f "$UTEST_REPORT"
-rm -f "$UTEST_LOG"
-# run tests and copy artifacts
-set -o pipefail
-RETURNCODE=0
-ctest --test-dir "$CMAKE_BUILD_DIR" 2>&1 | tee "$UTEST_REPORT" || RETURNCODE=$?
+TEST_DIR="$CMAKE_BUILD_DIR"
+mkdir -p "$RESULT_DIR/unit_test"
+cd "$RESULT_DIR/unit_test"
 
-tee "$UTEST_LOG" <"$CMAKE_BUILD_DIR"/Testing/Temporary/LastTest.log
+# shellcheck disable=SC2086 # Intended splitting of TESTS_REGEX.
+ctest --output-on-failure --force-new-ctest-process --verbose \
+    --output-junit "$RESULT_DIR/unit_test/junit.xml" \
+    --no-compress-output \
+    --output-log "$RESULT_DIR/unit_test/Test.log" \
+    --test-dir "$TEST_DIR" \
+    ${TESTS_REGEX}
+RETURNCODE=$?
+
+cp -r "${CMAKE_BUILD_DIR}/Testing/Temporary" "${RESULT_DIR}/unit_test/"
+
+TEST_LOG_FILE="${TEST_DIR}/Testing/Temporary/LastTest.log"
+SKIPPED_TESTS=$(sed -n -e '/^# skip/p' "${TEST_LOG_FILE}" | wc -l)
+if [ "${SKIPPED_TESTS}" -gt 0 ]; then
+    echo "Skipped tests (${SKIPPED_TESTS}):"
+    grep "# skip " "${TEST_LOG_FILE}"
+fi
+
 exit $RETURNCODE
